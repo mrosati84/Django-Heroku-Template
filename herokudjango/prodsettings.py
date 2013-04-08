fbdbf4e28eb4f58a5c099e2ef47d09dbf30c3c01
Matteo Rosati <mrosati@h-art.it>
2013-04-08 18:41:52 +0200

    env-settings file

---
+++ herokudjango/prodsettings.py
@@ -0,0 +1,19 @@
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ('127.0.0.1', 'localhost', 'http://fierce-sea-6758.herokuapp.com/')
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '*@ov6)8kv-l@9)4-zkuuy!!&amp;f2qxw1^&amp;10nu65b^&amp;4jno6zo#e'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
