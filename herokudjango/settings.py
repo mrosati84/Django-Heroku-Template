2202ea0ce0bdc587d756a7a1f9e76c9a1d8dd718
Matteo Rosati <mrosati@h-art.it>
2013-04-08 18:42:10 +0200

    settings (ignore local + imports)

--- herokudjango/settings.py
+++ herokudjango/settings.py
@@ -1,24 +1,14 @@
 # Django settings for herokudjango project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+import os
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+if os.getenv('ENV') == 'prod':
+    # import production settings from heroku
+    from herokudjango.prodsettings import *
+else:
+    # import local settings
+    from herokudjango.localsettings import *
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -45,18 +35,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -76,9 +66,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '*@ov6)8kv-l@9)4-zkuuy!!&amp;f2qxw1^&amp;10nu65b^&amp;4jno6zo#e'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
